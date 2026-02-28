381c4d64b6a3ea3f7b17a43259b7710801c265f6
Hasini Duggirala <sirihasiniduggirala06@gmail.com>
2026-02-28 13:34:46 -0600

    Homepage Added

---
+++ app/components/Mypage.jsx
@@ -0,0 +1,15 @@
+export default function Mypage(){
+    return(
+        <div>
+            <header>
+                <hi>Hey!! Welcome to my website :) </hi>
+            </header>
+            <p> 
+              I am Hasini Duggirala. I am a freshmen at uic majoring in computer science.
+              Really excited to build my own website.
+            </p>
+
+            <img src="/chicago.png" alt="CHICAGO" width={300}/>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import Mypage from "./components/Mypage";
 
 export default function Home() {
   return (
+    <div>
     <div className="flex flex-row gap-4">
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
       <Link href="/projects">Projects</Link>
+    </div>
+    <Mypage />
     </div>
   );
 }
